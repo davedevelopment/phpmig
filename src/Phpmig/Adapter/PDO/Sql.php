b490ab431540ca61b26451bf33d0200bde5cc7f4
sam <sam@Pluto.local>
2013-01-18 15:00:54 +0100

    add pdo sql adapter

---
+++ src/Phpmig/Adapter/PDO/Sql.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Phpmig\Adapter\PDO;
+
+use \Phpmig\Migration\Migration,
+    \Phpmig\Adapter\AdapterInterface;
+
+class Sql implements Phpmig\Adapter\AdapterInterface
+{
+
+    /**
+     * @var \PDO
+     */
+    protected $connection = null;
+
+    /**
+     * @var string
+     */
+    protected $tableName = null;
+
+
+
+    /**
+     * Constructor
+     *
+     * @param \PDO $connection
+     * @param string $tableName
+     */
+    public function __construct(\PDO $connection, $tableName)
+    {
+        $this->connection = $connection;
+        $this->tableName  = $tableName;
+    }
+
+    private function quotedTableName()
+    {
+        return $this->connection->quote($this->tableName);
+    }
+
+    /**
+     * Fetch all
+     *
+     * @return array
+     */
+    public function fetchAll()
+    {
+        $sql = "SELECT `version` FROM {$this->quotedTableName()} ORDER BY `version` ASC";
+        return $this->connection->query($sql, PDO::FETCH_COLUMN)->fetchAll();
+    }
+
+    /**
+     * Up
+     *
+     * @param Migration $migration
+     * @return self
+     */
+    public function up(Migration $migration)
+    {
+        $sql = "INSERT into {$this->quotedTableName()} set version = :version";
+        $this->connection->prepare($sql)
+                ->execute(array(':version' => $migration->getVersion()));
+        return $this;
+    }
+
+    /**
+     * Down
+     *
+     * @param Migration $migration
+     * @return self
+     */
+    public function down(Migration $migration)
+    {
+        $sql = "DELETE from {$this->quotedTableName()} where version = :version";
+        $this->connection->prepare($sql)
+                ->execute(array(':version' => $migration->getVersion()));
+        return $this;
+    }
+
+
+    /**
+     * Is the schema ready?
+     *
+     * @return bool
+     */
+    public function hasSchema()
+    {
+        $tables = $this->connection->query("show tables");
+        while($table = $tables->fetchColumn()) {
+            if ($table == $this->tableName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Create Schema
+     *
+     * @return DBAL
+     */
+    public function createSchema()
+    {
+        $sql = "CREATE table {$this->quotedTableName()} (version %s NOT NULL)";
+        $driver = $this->connection->getAttribute(PDO::ATTR_DRIVER_NAME);
+        $sql = sprintf($sql,in_array($driver,array('mysql','pgsql'))? 'VARCHAR(255)' : '');
+        $this->connection->exec($sql);
+        return $this;
+    }
+
+}
+
